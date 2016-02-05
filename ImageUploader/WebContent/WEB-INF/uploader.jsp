c0124afa789e19de5ccf22995d4eab7e73167194
alex.ansimov <alex.ansimov.ua@gmail.com>
2016-02-05 12:16:07 +0200

    add maven

--- ImageUploader/WebContent/WEB-INF/uploader.jsp
+++ ImageUploader/WebContent/WEB-INF/uploader.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,18 +9,19 @@
 <title>title here</title>
 </head>
 <body>
-	 <form method="post" action="/ImageUploader/uploader1" enctype="multipart/form-data">
+	 <form method="post" action="/ImageUploader" enctype="multipart/form-data">
 		<input type="file" name="fileImg" >
 		<input type="submit" value="Upload">
 	</form>
 	<br><br>
+
+	<img alt="image" src="saved_image/image1.jpg" style="width:304px;height:228px;"> 
 	
-	<img alt="image" src="image1.jpg" style="width:304px;height:228px;">
-	
-	
-	<%-- <c:forEach items="${imagenames}" var="imagename">
-    	<img src="${pageContext.request.contextPath}/images/${imagename}">
-	</c:forEach> --%>
+	${path}
+	<c:forEach var="name" items="${pageScope.names}">
+		<p>${name}</p>
+    	<%--  <img src="${name} style="width:304px;height:228px;">  --%>
+	</c:forEach>  
 	
 </body>
 </html>
--- ImageUploader/src/ua/org/oa/ansimov/MyServlet.java
+++ ImageUploader/src/ua/org/oa/ansimov/MyServlet.java
@@ -2,6 +2,8 @@ package ua.org.oa.ansimov;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -31,8 +33,9 @@ public class MyServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		
-		String path_dir = "/home/alex/Oracle_Academy/workspace/ImageUploader/WebContent/";
-		String namePathOfImages = path_dir + "saved_image";
+		String path_dir = "/home/alex/git/JavaEE-projects/ImageUploader/WebContent/";
+		System.out.println(path_dir);
+		String namePathOfImages = path_dir + File.separator + "saved_image";
 		File saveDir = new File(namePathOfImages);
 		if (!saveDir.exists()) {
 			saveDir.mkdir();
@@ -41,10 +44,17 @@ public class MyServlet extends HttpServlet {
 			
 		Part part = request.getPart("fileImg");
 		String fileName = part.getSubmittedFileName();
-		String picturePath = namePathOfImages + File.separator + fileName;
-		part.write(picturePath);
-		/*String df = "dfvdf";
-		request.setAttribute("path", df);*/
+		if (fileName.matches(".+\\.(jpg|jpeg)")) {
+			String picturePath = namePathOfImages + File.separator + fileName;
+			part.write(picturePath);
+		}
+		List<String> names = new ArrayList<>();
+		names.add("saved_image/image2.jpg");
+		names.add("saved_image/image3.jpg");
+		names.add("saved_image/image4.jpg");
+		
+		request.setAttribute("path", "saved_image/");
+		request.setAttribute("names", names);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/uploader.jsp");
 		dispatcher.forward(request, response);
 			
